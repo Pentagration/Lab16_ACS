da1028b487fab9640aade80c8366351c4ac38feb
Colinr1994 <Colinr1994@gmail.com>
2019-04-21 23:29:01 +0700

    Revised methods. Working out bugs. Need to incorperate utf8 decode.

--- Lab16.py
+++ Lab16.py
@@ -4,44 +4,31 @@
 
 from urllib import request
 import sys
+import os
 
-req = request.Request('https://www.bbc.com/news/world-asia-47997727')
 
-resp = request.urlopen(req)
-
-# Error handling. If request fails, try again with user agent. If it fails again system exit.
-if resp.code != 200:
-
-    req = request.Request('https://github.com/facebook/react-native/issues/10471', headers={'User-Agent': 'Mozilla/5.0'})
-
-    resp = request.urlopen(req)
-
-    if resp.code != 200:
-        sys.exit("Request failed.")
+import urllib.request
+with urllib.request.urlopen("http://www.python.org") as url:
+    s = url.read()
 
 
 writeString = ''
-
-data = resp.read().decode('utf-8')
-data = data.split('<p>') # Delim is paragraph tag
-data.pop(0) # Removing whatever comes before the first paragraph
-for str in data:
-    str = '!' + str # Adding a exclamation point to the beginning of every new string.
-    str = str.split('</p>') # running delim as ending paragraph tag.
-
-#data.remove(-1) # Removing whatever comes after the last paragraph tag
-
-for str in data:
-    if str[0] == '!':
-        writeString = writeString + str + '\n'
+with urllib.request.urlopen('https://www.bbc.com/news/world-us-canada-48001382') as url:
+    data = url.read()
+data = data.readlines()
+for line in data:
+    if '<p>' in line:
+        while not '</p>' in line:
+            writeString = writeString + line
 
 
 
 
 
-file = open("/home/colin/Documents/School/PentagrationGithub/Lab16_ACS/index.html", "wt")
 
-file.write(writeString)
+#file = open("/home/colin/Documents/School/PentagrationGithub/Lab16_ACS/index.html", "wt")
 
-file.close()
+#ile.write(writeString)
+
+#file.close()
 print(writeString)
